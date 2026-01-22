6d2eeb6228796f15a690aa877fd905930bcca7a9
swagatamath <math.swagata007@gmail.com>
2026-01-22 21:42:14 +0530

    crud application

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import * as authController from "./auth.controller";
+import { validate } from "../../middleware/validate.middleware";
+import { registerSchema, loginSchema } from "./auth.schema";
 
-const router= Router();
+const router = Router();
 
-router.post("/register", authController.register);
-
-router.post("/login", authController.login);
+router.post("/register", validate(registerSchema), authController.register);
+router.post("/login", validate(loginSchema), authController.login);
 
 export default router;
